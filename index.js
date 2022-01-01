34c9931295185dfcfca3a44b3b23085c57cb3a95
Abhishek Sah <abhi.sah.97@gmail.com>
2022-01-01 20:49:16 +0530

    broadcast messages to all clients

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
   socket.on("disconnect", () => {
     console.log("user disconnected");
