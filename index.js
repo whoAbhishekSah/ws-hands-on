d343a5d92827cea560fccddbf8eaef11644288b0
Abhishek Sah <abhi.sah.97@gmail.com>
2022-01-01 18:45:33 +0530

    enable client and server with a websocket connection

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+//listen on the connection event for incoming sockets and log it to the console.
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(3000, () => {
