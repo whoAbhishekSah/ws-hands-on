65c5623588192c0800fc76920473b50642ab55d8
Abhishek Sah <abhi.sah.97@gmail.com>
2022-01-01 20:52:11 +0530

    broadcast messages to all clients when someone connects or disconnects

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ app.get("/", (req, res) => {
 //listen on the connection event for incoming sockets and log it to the console.
 io.on("connection", (socket) => {
   console.log("a user connected");
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+  io.emit("chat message", "a user connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
   });
   socket.on("disconnect", () => {
     console.log("user disconnected");
+    io.emit("chat message", "a user disconnected");
   });
 });
 
