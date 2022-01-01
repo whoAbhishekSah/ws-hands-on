71494e68d16244977c3d9ba00c089b41ca8b521b
Abhishek Sah <abhi.sah.97@gmail.com>
2022-01-01 18:38:46 +0530

    add a basic chat window html interface on root path

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
